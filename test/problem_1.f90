f000f611afb6e98cb9cd3abff4b37294a58a2da9
Jacob Williams <jacobwilliams@users.noreply.github.com>
2021-11-30 23:18:01 -0600

    cleanup for day 1

--- test/problem_1.f90
+++ test/problem_1.f90
@@ -4,7 +4,7 @@ use aoc_utilities
 
 implicit none
 
-integer,dimension(:),allocatable :: iarray, iarray2
+integer,dimension(:),allocatable :: iarray
 integer :: i, n
 
 iarray = read_file_to_integer_array('inputs/day1.txt')
@@ -16,14 +16,9 @@ end do
 
 write(*,*) '1A: number measurements larger than the previous: ', n
 
-allocate(iarray2(0))
-do i = 1, size(iarray)-2
-    iarray2 = [iarray2, sum(iarray(i:i+2))]
-end do
-
 n = 0
-do i = 2, size(iarray2)
-    if (iarray2(i)>iarray2(i-1)) n = n + 1
+do i = 2, size(iarray)-2
+    if (sum(iarray(i:i+2)) > sum(iarray(i-1:i+1))) n = n + 1
 end do
 write(*,*) '1B: number sums larger than the previous: ', n
 
